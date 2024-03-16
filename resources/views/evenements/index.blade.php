ea2644d1f7dbca9ff21ba454c14a4494d1d8ad6d
Selma <ss2005sousou@gmail.com>
2024-03-16 12:14:01 -0700

    selma_evenement_delete_update

--- resources/views/evenements/index.blade.php
+++ resources/views/evenements/index.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.solic')
 
 @section('content')
@@ -5,14 +6,14 @@
 
     <div class="container-fluid">
 
-        <h1 class="mt-4 text-white" > Mes evenements</h1>
+        <h1 class="mt-4 text-white" > Mes évènements</h1>
 
         <div class="card mb-4">
 
             <div class="card-header">
                 <button data-toggle="modal" data-target="#Ajout_modal" class="btn btn-primary"> <i
                         class="fa fa-plus"></i>
-                     Ajouter Evenement
+                     Ajouter Événement
                 </button>
             </div>
 
@@ -73,7 +74,7 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h3 class="modal-title " id="lineModalLabel">Ajouter evenement : </h3>
+                    <h3 class="modal-title " id="lineModalLabel">Ajouter évènement : </h3>
                 </div>
                 
                 <div class="modal-body">
@@ -83,34 +84,34 @@
                         @csrf
                         
                         <div class="form-group col-md-12 col-sm-12 col-lg-12 col-xl-12">
-                            <label class="label label-default" for="Nom_evenemt">Nom d'évenement </label>
-                            <input required type="string" value="{{ old('nom_evenemt') }}" name="nom" placeholder="Entrez le nom de l'evenement" class="form-control" id="Nom_evenemt">
+                            <label class="label label-default" for="Nom_evenemt">Nom d'évènement </label>
+                            <input required type="string" value="{{ old('nom_evenemt') }}" name="nom" placeholder="Entrez le nom de l'évènement" class="form-control" id="Nom_evenemt">
                         </div>
 
                         <div class="form-group col-md-6">
                             <label class="label label-default" for="DateDebut_evenemt">Date de debut </label>
-                            <input required type="datetime-local" value="{{ old('DateDebut_evenemt') }}" name="date debut" class="form-control" id="DateDebut_evenemt" placeholder="Entrez la date de debut de l'evenement">
+                            <input required type="datetime-local" value="{{ old('DateDebut_evenemt') }}" name="date debut" class="form-control" id="DateDebut_evenemt" placeholder="Entrez la date de debut de l'évènement">
                         </div>
 
                         <div class="form-group col-md-6">
                             <label class="label label-default" for="DateFin_evenemt">Date de fin </label>
-                            <input required type="datetime-local" value="{{ old('DateFin_evenemt') }}" name="date fin" class="form-control" id="DateFin_evenemt" placeholder="Entrez la date de fin de l'evenement">
+                            <input required type="datetime-local" value="{{ old('DateFin_evenemt') }}" name="date fin" class="form-control" id="DateFin_evenemt" placeholder="Entrez la date de fin de l'évènement">
                         </div>
 
                         <div class="form-group col-md-12">
                             <label class="label label-default" for="Description_evenemt">Description</label>
                             
-                            <textarea rows="4" class="form-control" name="description" placeholder="Entrez la description de l'évenement" id="Description_evenemt"></textarea>
+                            <textarea rows="4" class="form-control" name="description" placeholder="Entrez la description de l'évènement" id="Description_evenemt"></textarea>
                         </div>
 
-                        <div class="form-group col-md-6">
+                        <div class="form-group col-md-12">
                             <label class="label label-default" for="Lieu_evenemt">Lieu </label>
-                            <input required type="string" value="{{ old('Lieu_evenemt') }}" name="lieu" class="form-control" id="Lieu_evenemt" placeholder="Entrez le lieu de l'evenement">
+                            <input required type="string" value="{{ old('Lieu_evenemt') }}" name="lieu" class="form-control" id="Lieu_evenemt" placeholder="Entrez le lieu de l'évènement">
                         </div>
 
                         <div class="btn-group col-md-6" role="group">
                             
-                            <button type="submit" class="col-md-12 btn btn-primary">Save</button>
+                            <button type="submit" class="col-md-12 btn btn-primary">Sauvegarder</button>
                         </div>
 
                         <div class="btn-group col-md-6" role="group">
