01265c8da43f8ec3707408f0e9c7b2abd5bb599b
Meriem BENYAMMI <arwa.jasmine04@MacBook-Air-de-Meriem.local>
2024-03-18 21:26:28 +0100

    j'ai modifier les dates des migrations

--- app/Models/gestionnaire.php
+++ app/Models/gestionnaire.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Gestionnaire extends Model
 {
-    use HasFactory;
+    use HasFactory , SoftDeletes;
     public static function add (Request $request)
     {
         DB::insert("insert into gestionnaires(nom,prenom,date_naissance,genre,email,num_tel,num_tel_urgence,adresse,id_qr)
@@ -43,9 +44,9 @@ class Gestionnaire extends Model
     }
     public static function supprimer($id_gestionnaire)
     {
-        return DB::delete("delete from gestionnaires where id = \"$id_gestionnaire\" ");
+        $gestionnaire = Gestionnaire::find($id_gestionnaire);
+        $gestionnaire->delete();
         // code...
     }
 }
-
 
